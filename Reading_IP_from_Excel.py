162e9f55db2b62e11f432b8ba48d7394d969e6b7
Maheboob <maheboob.n.tumkur@gmail.com>
2019-09-11 18:38:59 +0530

    reading IP_Address and Names from text file, created dictionary and written to excel file

--- Reading_IP_from_Excel.py
+++ Reading_IP_from_Excel.py
@@ -1,25 +1,49 @@
 #importin required Libraries
+import os,glob
 import xlrd
 import openpyxl
 import pandas as pd
 
-# Creating an Empy Dictionary
+# Creating an Empty Dictionary
 d={}
 
-#importing data of excel file and storing it as a list
-df=pd.read_excel("C:\\Users\\Ifra\\Desktop\\xyz.xlsx","Sheet1")
-IP_add=df["IP Addresses"].values.tolist()
-name=df["Names"].values.tolist()
+#Reading text file
+f=open("C:\\Users\\Ifra Fatimah\\Desktop\\New folder (3)\\file_1\\file_2\\file_3\\1.txt","r")
+line = f.readlines()
+data2 = []
+for ele in line:
+    ele1 = ele.strip()
+    data2.append(ele1)
 
-#printing created list
-print(IP_add)
-print(name)
+data3 = data2[1:]
+for data in data3:
+    data4 = data.split(' ')
+    d[data4[0]] = data4[1]
 
-#Zipping two lists to make one file
-zipobj=zip(IP_add,name)
-
-#creating a dictionary from zipped file
-d=dict(zipobj)
-
-#printing dictionary
 print(d)
+L1=d.values()
+#print(L1)
+for l1 in L1:
+    print(l1)
+L2=d.keys()
+for l2 in L2:
+    print(l2)
+
+
+# #print(f.read())
+path="C:\\Users\\Ifra Fatimah\\Desktop\\IP_Address.xlsx"
+workbook=openpyxl.load_workbook(path)
+sheet=workbook.active
+r=1
+c=1
+for l2 in L2:
+    sheet.cell(row=r,column=c).value=l2
+    r +=1
+workbook.save(path)
+
+r=1
+c=2
+for l1 in L1:
+    sheet.cell(row=r,column=c).value=l1
+    r +=1
+workbook.save(path)
